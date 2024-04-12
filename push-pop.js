934b930d14c6ec93f080ad860457c5b1014c7c7a
SparksFlash <ug2102056@cse.pstu.ac.bd>
2024-04-12 09:22:03 +0600

    push-pop

--- push-pop.js
+++ push-pop.js
@@ -16,4 +16,17 @@ console.log(friendsAge);
 console.log(lastItem);
 
 // javascript remove first item from an array !!
+
+var fruits = ['Banana', 'Orange', 'Apple', 'Mango'];
+console.log(fruits.shift());
+
+var arr = [1, 2, 3, 4]; 
+var theRemovedElement = arr.shift(); // theRemovedElement == 1
+console.log(arr); // [2, 3, 4]
+
 // javascript array add element in the beginning !!!
+
+const array = [3, 2, 1]
+const newFirstElement = 4
+const newArray = [newFirstElement].concat(array) // [ 4, 3, 2, 1 ]
+console.log(newArray);
